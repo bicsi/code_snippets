413f80d8e0e263181dfd89e9a6d85d06559ba193
bicsi <bicsi>
2016-08-02 14:54:27 +0300

    Redid aho corasick

--- aho_corasick.cpp
+++ aho_corasick.cpp
@@ -1,67 +1,60 @@
 struct AhoCorasick {
- 
     struct Node {
-        int cnt, bad, adj, parent;
-        char c;
+        int link;
         map<char, int> leg;
     };
     vector<Node> T;
-    int root = 1, nodes = 1;
+    int root = 0, nodes = 1;
 
     AhoCorasick(int sz) {
         T.resize(sz);
     }
 
-    int addWord(const string &word) {
+    // Adds a word to trie and returns the end node
+    int AddWord(const string &word) {
         int node = root;
         for(auto c : word) {
-            
+
             auto &nxt = T[node].leg[c];
 
-            if(nxt == 0) {
-                nxt = ++nodes;
-                T[nodes].parent = node;
-                T[nodes].c = c;
-            }
+            if(nxt == 0) 
+                nxt = nodes++;
 
             node = nxt;
         }
      
         return node;
     }
-     
-    queue<int> Q;
-    void buildBad() {
-     
+
+    // Advances from a node with a character (like an automaton)
+    int Advance(int node, char chr) {
+        while(node != -1 && T[node].leg.count(chr) == 0)
+            node = T[node].link;
+        
+        if(node == -1)
+            return root;
+        
+        return T[node].leg[chr];
+    }
+
+    // Builds links
+    void BuildLinks() {
+        
+        queue<int> Q;
         Q.push(root);
+        T[root].link = -1;
+        
         while(!Q.empty()) {
             int node = Q.front();
             Q.pop();
-     
-            
-            int bad = T[T[node].parent].bad;
-            char c = T[node].c;
- 
-            while(bad && T[bad].leg.find(c) == T[bad].leg.end()) 
-                bad = T[bad].bad;
 
-            T[node].bad = (node == root) ? 0 : (bad) ? T[bad].leg[c] : root;
+            for(auto &p : T[node].leg) {
+                int vec = p.second;
+                char chr = p.first;
 
-            for(auto p : T[node].leg)
-                Q.push(p.second);
-        }
-    }
-
-    void parseText(const string &text) {
-        int node = root;
-
-        for(auto c : text) {
-            while(node && T[node].leg.find(c) == T[node].leg.end())
-                node = T[node].bad;
-            node = node ? T[node].leg[c] : root;
-
-            // Here you go
+                T[vec].link = Advance(T[node].link, chr);
+                Q.push(vec);
+            }
         }
     }
 };
-
