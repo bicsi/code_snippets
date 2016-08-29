b67445484943730d85bbbe3d33738a002761d367
Bicsi <bicsi@ymail.com>
2016-08-29 17:03:13 +0300

    Fixed Biconnected components

--- biconnected.cpp
+++ biconnected.cpp
@@ -1,16 +1,17 @@
 namespace CBC {
 	vector<pair<int, int>> St;
-	int Parent[kMaxN], Disc[kMaxN], Low[kMaxN];
-	vector<int> CBC[kMaxN];
-	bool Viz[kMaxN], Art[kMaxN];
+	int Parent[kMaxN], Time[kMaxN], Low[kMaxN];
+	vector<int> Nodes[kMaxN];
+	bool Viz[kMaxN];
+	int Art[kMaxN];
 	int n, timer, cbc;
 
 	void Init(int nn) {
 		n = nn;
 
 		for(int i = 1; i <= n; ++i) {
-			Parent[i] = Disc[i] = Low[i] = Viz[i] = Art[i] = 0;
-			CBC[i].clear();
+			Parent[i] = Time[i] = Low[i] = Viz[i] = Art[i] = 0;
+			Nodes[i].clear();
 		}
 
 		timer = cbc = 0;
@@ -19,32 +20,35 @@ namespace CBC {
 
 	void DFS(int node) {
 	    Viz[node] = 1;
-	    Disc[node] = ++timer;
-	    Low[node] = Disc[node];
+	    Time[node] = ++timer;
+	    Low[node] = Time[node];
 	 
-	    for(vector<int>::iterator it = G[node].begin(); it != G[node].end(); ++it) {
-	        const int &vec = *it;
+	    for(auto vec : G[node]) {
 	        if(vec == Parent[node]) continue;
 	 
 	        if(!Viz[vec]) {
 	            Viz[vec] = 1;
 	            Parent[vec] = node;
 	            St.emplace_back(node, vec);
+
 	            DFS(vec);
 	 
 	            Low[node] = min(Low[node], Low[vec]);
 	 
-	            if(Low[vec] >= Disc[node]) {
+	            if(Low[vec] >= Time[node]) {
 	                cbc++;
-	                Art[node] = 1;
-	 
-	                while(St.back() != make_pair(node, vec) ) {
-	                    CBC[cbc].push_back(St.back().second);
-	                    St.pop_back();
-	                }
-	                CBC[cbc].push_back(node);
-	                CBC[cbc].push_back(vec);
-	                St.pop_back();
+	 				
+	 				while(true) {
+	 					auto p = St.back();
+	 					St.pop_back();
+
+	 					Nodes[cbc].push_back(p.second);
+
+	 					if(p.first == node)
+	 						break;
+	 				}
+	 				
+	 				Nodes[cbc].push_back(node);
 	            }
 	        } else if(Low[vec] < Low[node]) {
 	            Low[node] = Low[vec];
@@ -52,14 +56,13 @@ namespace CBC {
 	    }
 	}
 
-	void Start(int start) {
-		if(G[start].empty()) return;
-		DFS(start);
+	void MakeArticulationPoints() {
+		for(int i = 1; i <= cbc; ++i) {
+			for(auto node : Nodes[i])
+				Art[node] += 1;
+		}
 
-		Art[start] = 0;
-		int x = G[start].back();
-		for(auto y : G[start])
-			if(y != x)
-				Art[start] = 1;
+		for(int i = 1; i <= n; ++i)
+			Art[i] = (Art[i] >= 2);
 	}
 };
