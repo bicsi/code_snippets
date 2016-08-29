bc961028d5fcef6be62c6c13b75272722c257ae4
Bicsi <bicsi@ymail.com>
2016-08-29 20:23:17 +0300

    Added namespace to ModOps

--- modops.cpp
+++ modops.cpp
@@ -1,30 +1,31 @@
 const int kMod = 1e9 + 7;
 
-int add(int a, int b) {
-	a += b;
-	if(a >= kMod) a -= kMod;
-	return a;
-}
-int sub(int a, int b) {
-	a -= b;
-	if(a < 0) a += kMod;
-	return a;
-}
-int mul(int a, int b) {
-	return 1LL * a * b % kMod;
-}
-int lgpow(int b, int e) {
-	int r = 1;
-	while(e) {
-		if(e % 2) 
-			r = mul(r, b);
-		b = mul(b, b);
-		e /= 2;
+namespace ModOps {
+	int add(int a, int b) {
+		a += b;
+		if(a >= kMod) a -= kMod;
+		return a;
 	}
-	return r;
-}
-int inv(int x) {
-	int ret = lgpow(x, kMod - 2);
-	assert(mul(ret, x) == 1);
-	return ret;
-}
+	int sub(int a, int b) {
+		a -= b;
+		if(a < 0) a += kMod;
+		return a;
+	}
+	int mul(int a, int b) {
+		return 1LL * a * b % kMod;
+	}
+	int lgpow(int b, int e) {
+		int r = 1;
+		while(e) {
+			if(e % 2) 
+				r = mul(r, b);
+			b = mul(b, b);
+			e /= 2;
+		}
+		return r;
+	}
+	int inv(int x) {
+		return lgpow(x, kMod - 2);
+	}
+};
+using namespace ModOps;
