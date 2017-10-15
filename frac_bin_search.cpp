475eea418d9eb05b1eef8932e8e81a45d3cf9754
lucianbicsi <lucianbicsi@gmail.com>
2017-10-15 22:03:04 +0300

    Changed frac bin search to do log instead of log^2

--- frac_bin_search.cpp
+++ frac_bin_search.cpp
@@ -6,30 +6,27 @@ struct Frac {
 // the numerator, given a predicate pred. The predicate should
 // monotonous function going from negative to 0 to positive.
 // The function will find a random fraction f for which pred(f) = 0.
-// Complexity: O(log^2(max_num))
+// Complexity: O(log(max_num))
 template<typename Predicate>
 Frac FracBinarySearch(long long max_num, Predicate pred) {
     Frac lo{0, 1}, hi{1, 1}; // set to {1, 0} for [0...max_num)
     int sign = 1;
     
-    long long maxx = 1;
-    while (maxx <= max_num / 2)
-        maxx *= 2;
-
-    if (pred(lo) == 0) return lo;
     while (true) {    
-        long long adv = 0;
-        for (long long step = maxx; step; step /= 2) {
+        int adv = 0;
+        bool down = false;
+
+        for (int step = 1; step; down ? step /= 2 : step *= 2) {
             adv += step;
 
             Frac mid{lo.a * adv + hi.a, lo.b * adv + hi.b};
             if (mid.a > max_num or mid.b > max_num) {
-                adv -= step; continue;
+                adv -= step; down = true; continue;
             }
 
-            int res = pred(mid);
+            long long res = pred(mid);
             if (res == 0) return mid;
-            if (res * sign < 0) adv -= step;
+            if (res * sign < 0) { adv -= step; down = true; }
         }  
 
         hi.a += lo.a * adv; 
